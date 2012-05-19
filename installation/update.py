74ca35f0757839c41a5b6c50f2f786c4a6e4c433
criloz <criscalovis@gmail.com>
2012-05-19 14:36:26 -0500

    now always restart the server

--- installation/update.py
+++ installation/update.py
@@ -8,8 +8,7 @@ if __name__=="__main__":
     #git pull
     msg =  os.popen("/usr/bin/env git pull origin master").read()
     msg = msg.splitlines()
-    if len(msg)>1:
-        #stop
-        print os.popen("/usr/bin/env python "+folder+"stop_verifiers.py").read()
-        #start
-        os.execl("/usr/bin/env","python",folder+"start_verifiers.py")
+    #stop
+    print os.popen("/usr/bin/env python "+folder+"stop_verifiers.py").read()
+    #start
+    os.execl("/usr/bin/env","python",folder+"start_verifiers.py")
