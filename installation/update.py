0d5b49a7941e9832f0ce399e9131d0e63fd46bc5
criloz <criscalovis@gmail.com>
2012-05-22 12:12:23 -0500

    updates

--- installation/update.py
+++ installation/update.py
@@ -13,13 +13,13 @@ if __name__=="__main__":
     
     os.popen("chmod 777 -R /home/verifiers").read()
     os.popen("chown verifiers:verifiers -R /home/verifiers").read()
-    """
     #cd
     os.chdir(folder)
     #git pull
+    msg =  os.popen("/usr/bin/env git reset --hard HEAD").read()
     msg =  os.popen("/usr/bin/env git pull origin master").read()
     msg = msg.splitlines()
     #stop
     print os.popen("/usr/bin/env python "+folder+"stop_verifiers.py").read()
     #start
-    os.execl("/usr/bin/env","python",folder+"start_verifiers.py")"""
+    os.execl("/usr/bin/env","python",folder+"start_verifiers.py")
