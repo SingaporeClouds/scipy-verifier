2ac5638d28005fd6a7e9d1d4bc38f1f8af9e8f18
criloz <criscalovis@gmail.com>
2012-09-27 04:09:43 -0500

    finishing scripts

--- start_verifiers.py
+++ start_verifiers.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python
+import json
 # start the server
-import os,sys
+import os,sys,pwd
 folder = os.path.dirname(os.path.abspath(__file__))
-os.chdir(folder)
+verifiers_home_dir = pwd.getpwnam("verifiers").pw_dir
 #clean ports
 print "closing ports"
-os.popen("fuser -k 80/tcp").read()
-os.popen("fuser -k 2012/tcp").read()
+print os.popen("fuser -k 80/tcp").read()
+print os.popen("fuser -k 2012/tcp").read()
 #compile and run java server
+print os.popen("python "+folder+"/compile_java.py").read()
 
 #run python server
-output = str(os.spawnv(os.P_NOWAIT,sys.executable,("python",folder+"/server.py","")))
-_file = open("pid","w")
-_file.write(output)
+output = str(os.spawnv(os.P_NOWAIT,sys.executable,("python",folder+"/python_server/server.py","")))
+pw_record      = pwd.getpwnam("verifiers")
+user_name      = pw_record.pw_name
+user_home_dir  = pw_record.pw_dir
+user_uid       = pw_record.pw_uid
+user_gid       = pw_record.pw_gid
+os.environ['HOME']     = user_home_dir
+os.environ['LOGNAME']  = user_name
+os.environ['USER']     = user_name
+os.environ['LANGUAGE'] = "en_US.UTF-8"
+os.environ['LANG']     = "en_US.UTF-8"
+os.environ['LC_ALL']   = "en_US.UTF-8"
+os.setgid(user_gid)
+os.setuid(user_uid)
+os.chdir(verifiers_home_dir+"/javaserver/build/classes/")
+java_pid = str(os.spawnv(os.P_NOWAIT,"/usr/bin/java",("java","-cp",":"+verifiers_home_dir+"/javaserver/libs/*","com/singpath/javabox/Server")))
+
+
+
+pids = {"java":java_pid,"python":output}
+os.chdir("/tmp")
+_file = open("singpath_pids","w+")
+_file.write(json.dumps(pids))
 _file.close()
 print "server opened"
--- stop_verifiers.py
+++ stop_verifiers.py
@@ -3,18 +3,25 @@
 
 import os
 import signal
+import json
 folder = os.path.dirname(os.path.abspath(__file__))
-os.chdir(folder)
-
-_file = open("pid","r")
-pid = int(_file.read().strip())
-_file.close()
+os.chdir("/tmp")
 
 try:
-    os.kill(pid,signal.SIGKILL)
+    _file = open("singpath_pids","r")
+    content = _file.read()
+    _file.close()
+    pid = json.loads(_file.read().strip())
+except:
+    pid = {}
+
+try:
+    os.kill(int(pid["java"]),signal.SIGKILL)
+    os.kill(int(pid["python"]),signal.SIGKILL)
 except:
     pass
 
 os.popen("fuser -k 80/tcp").read()
+os.popen("fuser -k 2012/tcp").read()
 
 print " Server closed"
